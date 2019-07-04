4480e4063c100ba0ce1e74f99a3879f97c9352a8
Ernest Delgado <ernestdelgado8@gmail.com>
2019-07-03 20:51:34 -0400

    Zombie Rush Single Play

---
+++ Assets/Scripts/Object.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object : MonoBehaviour
+{
+    [SerializeField] float objectSpeed = 1;
+    [SerializeField] private float resetPosition = 30.0f;
+    [SerializeField] private float startPosition = -80.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        if (!GameManager.instance.GameOver)
+        {
+            transform.Translate(Vector3.right * (objectSpeed * Time.deltaTime));
+
+            if (transform.localPosition.x >= resetPosition)
+            {
+                Vector3 newPos = new Vector3(startPosition, transform.position.y, transform.position.z);
+                transform.position = newPos;
+            }
+
+
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Rock.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : Object
+{
+
+    [SerializeField] Vector3 topPosition;
+    [SerializeField] Vector3 bottomPosition;
+    [SerializeField] float rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Move(bottomPosition));
+    }
+
+    protected override void Update()
+    {
+        
+        if (GameManager.instance.PlayerActive)
+        {
+            base.Update();
+        }
+        
+    }
+
+    
+
+    IEnumerator Move(Vector3 target)
+    {
+        while (Mathf.Abs((target - transform.localPosition).y) > 0.20f) {
+            Vector3 direction = target.y == topPosition.y ? Vector3.up : Vector3.down;
+            transform.localPosition += direction * Time.deltaTime;
+
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        Vector3 newTarget = target.y == topPosition.y ? bottomPosition : topPosition;
+        StartCoroutine(Move(newTarget));
+    }
+}
